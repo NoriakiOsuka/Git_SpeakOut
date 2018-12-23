7b1a1c55ede40669220bd244fce9cecd98ec4931
EC2 Default User <ec2-user@ip-172-31-29-209.ap-southeast-1.compute.internal>
2018-12-23 02:16:02 +0000

    usermodelテーブル作成、ルーティング記載

--- db/migrate/20181223014125_devise_create_ad_users.rb
+++ db/migrate/20181223014125_devise_create_ad_users.rb
@@ -14,23 +14,31 @@ class DeviseCreateAdUsers < ActiveRecord::Migration[5.2]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      # Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      # Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+      
+      t.string  :family_name
+      t.string  :first_name
+      t.string  :family_name_kana
+      t.string  :first_name_kana
+      t.string  :nickname
+      t.string  :occupation
+      t.string  :position
 
 
       t.timestamps null: false
