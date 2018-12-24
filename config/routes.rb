4cda23cafe4c8db07cd07afd107771090b1192e8
EC2 Default User <ec2-user@ip-172-31-29-209.ap-southeast-1.compute.internal>
2018-12-24 00:55:37 +0000

    複数モデルのストロングパラメーター設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :ad_users
-  devise_for :st_users
+  devise_for :ad_users, path: 'ad_users' ,controllers: {
+    sessions: 'ad_users/sessions',
+    registrations: 'ad_users/registrations'
+  }
+  devise_for :st_users, path: 'st_users' ,controllers: {
+    sessions: 'st_users/sessions',
+    registrations: 'st_users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top#index"
   resources :ad_users,　only: [:show, :edit, :update]
